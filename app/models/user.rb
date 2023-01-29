ddd90579a27b9dd6886fca28c41c3f45bad7008f
selma <belhadjselma1@gmail.com>
2023-01-29 23:54:31 +0100

    Added validations to the user model and user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@
 # password_confirmation: string virtual
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_221038) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
